34c2aeb62ab14647a9269e53e4ba0ac8c7fd59ac
lemonsevens <matthew2morris@gmail.com>
2025-07-14 19:22:40 -0400

    Refactor theme management by removing ThemeSelect components and updating configuration. Adjust CSS classes for improved styling consistency across various components. Enhance layout and responsiveness in multiple sections, including Navbar, Footer, and Testimonials, while removing outdated elements for a cleaner structure.

--- astro.config.mjs
+++ astro.config.mjs
@@ -48,7 +48,7 @@ export default defineConfig({
       SiteTitle: "./src/components/ui/starlight/SiteTitle.astro",
       Head: "./src/components/ui/starlight/Head.astro",
       MobileMenuFooter: "./src/components/ui/starlight/MobileMenuFooter.astro",
-      ThemeSelect: "./src/components/ui/starlight/ThemeSelect.astro",
+      // ThemeSelect: "./src/components/ui/starlight/ThemeSelect.astro",
     },
     head: [
       {
